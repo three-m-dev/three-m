306a82b904fb302ab7cfa38b366748096c042c22
Jacob Reppuhn <jreppuhn@three-m.com>
2024-03-04 13:24:54 -0500

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,8 +32,8 @@ const Navbar = () => {
           <div className='flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse'>
             <Link
               to='/contact'
-              className='text-white bg-primary hover:bg-secondary focus:outline-none font-medium rounded-lg px-4 py-2 text-center flex items-center'>
-              Contact Us
+              className='text-white bg-primary hover:bg-secondary focus:outline-none font-medium rounded-lg px-4 py-2 text-center flex items-center uppercase'>
+              Contact
             </Link>
             <button
               data-collapse-toggle='navbar-sticky'
@@ -62,7 +62,7 @@ const Navbar = () => {
           <div
             className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
             id='navbar-sticky'>
-            <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white'>
+            <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white uppercase'>
               <li>
                 <NavLink
                   to='/'
