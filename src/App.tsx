1db0e0f082d50e2d383e8b1922360ad8735e328e
Jacob Reppuhn <jreppuhn@three-m.com>
2024-03-04 10:38:38 -0500

    add some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,73 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {
-	About,
-	Bulletin,
-	BulletinManager,
-	BulletinPost,
-	CareerManager,
-	Careers,
-	Contact,
-	Home,
-	Login,
-	NotFound,
-	Services,
+  About,
+  Bulletin,
+  BulletinManager,
+  BulletinPost,
+  CareerManager,
+  Careers,
+  Contact,
+  Home,
+  Login,
+  NotFound,
+  Services,
 } from './pages';
+import ScrollToTop from './utils/ScrollToTop';
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/services" element={<Services />} />
-				<Route path="/careers" element={<Careers />} />
-				<Route path="/bulletin" element={<Bulletin />} />
-				<Route path="/bulletin/*" element={<BulletinPost />} />
-				<Route path="/contact" element={<Contact />} />
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='/about'
+          element={<About />}
+        />
+        <Route
+          path='/services'
+          element={<Services />}
+        />
+        <Route
+          path='/careers'
+          element={<Careers />}
+        />
+        <Route
+          path='/bulletin'
+          element={<Bulletin />}
+        />
+        <Route
+          path='/bulletin/*'
+          element={<BulletinPost />}
+        />
+        <Route
+          path='/contact'
+          element={<Contact />}
+        />
 
-				<Route path="/dashboard/login" element={<Login />} />
-				<Route path="/dashboard/careers" element={<CareerManager />} />
-				<Route path="/dashboard/bulletin" element={<BulletinManager />} />
+        <Route
+          path='/dashboard/login'
+          element={<Login />}
+        />
+        <Route
+          path='/dashboard/careers'
+          element={<CareerManager />}
+        />
+        <Route
+          path='/dashboard/bulletin'
+          element={<BulletinManager />}
+        />
 
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
-	);
+        <Route
+          path='*'
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
