e2c1eaade7db8c68a4320841b490e6cb754c274f
Jacob Reppuhn <jreppuhn@three-m.com>
2024-03-04 13:38:50 -0500

    animate stats

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,19 +1,49 @@
+import { useEffect, useState } from 'react';
+
 const Stats = () => {
+  const animateValue = (target: number, setDisplayValue: React.Dispatch<React.SetStateAction<number>>) => {
+    const duration = 500;
+    let startValue = 0;
+    const stepTime = 20;
+    const steps = duration / stepTime;
+    const valueIncrement = target / steps;
+    let currentValue = startValue;
+
+    const intervalId = setInterval(() => {
+      currentValue += valueIncrement;
+      if (currentValue >= target) {
+        clearInterval(intervalId);
+        currentValue = target;
+      }
+      setDisplayValue(Math.floor(currentValue));
+    }, stepTime);
+  };
+
+  const [yearsInBusiness, setYearsInBusiness] = useState(0);
+  const [projectsCompleted, setProjectsCompleted] = useState(0);
+  const [industriesServed, setIndustriesServed] = useState(0);
+
+  useEffect(() => {
+    animateValue(50, setYearsInBusiness);
+    animateValue(5000, setProjectsCompleted);
+    animateValue(25, setIndustriesServed);
+  }, []);
+
   return (
     <section className='bg-primary'>
       <div className='max-w-screen-xl px-4 py-8 mx-auto text-center lg:px-6'>
         <dl className='grid max-w-screen-lg mx-auto text-white sm:grid-cols-3'>
           <div className='flex flex-col items-center justify-center'>
-            <dt className='mb-2 text-3xl md:text-4xl font-extrabold'>50+</dt>
-            <dd className='font-light text-white'>Years of Business</dd>
+            <dt className='mb-2 text-3xl text-white sm:text-5xl font-bebas tracking-wider'>{yearsInBusiness}+</dt>
+            <dd className='text-white'>Years In Business</dd>
           </div>
           <div className='flex flex-col items-center justify-center'>
-            <dt className='mb-2 text-3xl md:text-4xl font-extrabold'>5000+</dt>
-            <dd className='font-light text-white'>Projects Completed</dd>
+            <dt className='mb-2 text-3xl text-white sm:text-5xl font-bebas tracking-wider'>{projectsCompleted}+</dt>
+            <dd className='text-white'>Projects Completed</dd>
           </div>
           <div className='flex flex-col items-center justify-center'>
-            <dt className='mb-2 text-3xl md:text-4xl font-extrabold'>25+</dt>
-            <dd className='font-light text-white'>Industries Served</dd>
+            <dt className='mb-2 text-3xl text-white sm:text-5xl font-bebas tracking-wider'>{industriesServed}+</dt>
+            <dd className='text-white'>Industries Served</dd>
           </div>
         </dl>
       </div>
